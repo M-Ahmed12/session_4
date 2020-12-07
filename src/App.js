0565d30d21ff5efe0c7b253db2dce7ed1aa97399
Ahmed Shahid <Ahmed_shahid50@yahoo.com>
2020-12-07 18:44:10 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, {useState} from 'react';
-import {Message} from './Message.js'
+import {Message} from './Message.js';
 import './App.css';
 
 export default function App() {
   let [count, setCount] = useState(0);
   let [isMorning, setMorning] = useState(false)
+
   
   return (
-    <div className= {`box ${isMorning ? 'daylight' : ''} `}>
-    
-    <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
-    
-    
-    <Message counter={count}/>
-    <br/>
-    <button onClick={()=> setCount(count + 1) }> update counter </button>
-    <button onClick={()=> setMorning(!isMorning)} > Update Day</button>
+    <div className= {`box ${isMorning ? 'daylight' : ''}`}>
+      <h1>have a good {isMorning ? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count}/>
+      <br/>
+      <button onClick={()=> setCount(count + 1)}> update counter</button>
+      <button onClick={()=> setMorning(!isMorning)}> update day </button>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -2,5 +2,5 @@ import React from 'react';
 export function Message (props){
     return(
     <h3>value of counter variable is: {props.counter}</h3>
-    )
+        )
 }
